9509a47d3f841d97966ed97eca1c61cc8deb2e1d
Koumpotis Christos <chkoumpotis@davidson.edu>
2020-02-10 14:06:37 -0800

    numbers milestone

---
+++ numbers.js
@@ -0,0 +1,25 @@
+let num1 = 10
+let num2 = 20
+
+console.log('addition' ,num1 + num2 );
+console.log('subtraction' , num1-num2);
+console.log('multiplication', num1 * num2 );
+console.log('division', num1/num2);
+console.log('module/ remainder of division of 2 numbers', num1%num2);
+console.log('power of num1 to the num2', num1**num2);
+console.log('the result as an integer, stripped out of its decimals', Math.floor(num1/num2));
+let blah = 24
+console.log('blah =' , blah);
+console.log('right most digit:', blah%20);
+console.log('is even if blah%2=0. blah%2=', blah%2);
+console.log('increment by 1', blah+1);
+console.log('increment by 2', blah+2);
+console.log('increment by 10', blah+10);
+let k=4;
+console.log('k is', k);
+console.log('increment by k', blah+k);
+console.log('------NOW WE DECREMENT-------');
+console.log('decrement by 1', blah-1);
+console.log('decrement by 2', blah-2);
+console.log('decrement by 10', blah-10);
+console.log('decrement by k', blah-k);
