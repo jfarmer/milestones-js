f909f6ec0e6286358adce4252137062e89e16a1b
Patrick Sullivan <pasullivan@davidson.edu>
2020-02-10 22:53:41 -0800

    strings

---
+++ strings.js
@@ -0,0 +1,40 @@
+let str1 = "fuddy";
+let str2 = "duddy";
+console.log(str1.concat('', str2));
+console.log(str2.includes(str1));
+let waffles = "pancakes";
+let griddle = waffles.length;
+console.log(griddle);
+console.log(waffles.toUpperCase());
+console.log(waffles.toLowerCase());
+let zero = waffles.charAt(0);
+console.log(zero);
+let one = waffles.charAt(1);
+console.log(one);
+let last = waffles.charAt(waffles.length - 1);
+console.log(last);
+let k = 3;
+let returnK = waffles.charAt(k);
+console.log(returnK);
+
+
+let dogName = "Fido";
+
+console.log("Fetch, " + dogName + "!");
+
+/*let appleWaffles = waffles + "a";
+console.log(appleWaffles);
+  - [ ] Print the values of `str1` and `str2`
+  - [ ] Concatenate `str1` and `str2`
+  - [ ] Determine whether `str1` appears in `str2` using `String.prototype.includes`
+- [ ] Assign a string to a variable named `waffles` and...
+  - [ ] Get the number of characters in the string (its length)
+  - [ ] Convert it to uppercase
+  - [ ] Convert it to lowercase
+  - [ ] Get the 1st character in `waffles` (at index `0`)
+  - [ ] Get the 2nd character in `waffles` (at index `1`)
+  - [ ] Get the last character in `waffles`
+  - [ ] Define a variable `k` containing a number and get the character at index `k` in `waffles`
+  - [ ] Modify the value of `waffles` by appending the letter `'a'` to it
+- [ ] Use template literals to print a formatted string containing dynamic information, e.g., if `firstName` is `'Jesse'`, use a template literal to print `'Hello, Jesse!'`.
+*/
