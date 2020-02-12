34b3fb63fbb2c7b11d02d75ccd7d5b4b3ce3c34a
Jalen Staples <jastaples@davidson.edu>
2020-02-11 23:17:39 -0800

    2nd activity

---
+++ strings.js
@@ -0,0 +1,24 @@
+let str1='hey';
+let str2='bye';
+console.log(str1,str2);
+
+function string(string1,string2) {
+let str3=string1 + string2
+  console.log(string1 + string2);
+}
+console.log(string('mom', 'dad'));
+console.log(str1.includes(str2));
+
+ string2='waffles';
+console.log(string2.length);
+console.log (string2.toUpperCase());
+console.log(string2.toLowerCase());
+console.log(string2.charAt(0));
+console.log(string2.charAt(1));
+console.log(string2.charAt(6));
+console.log(string2.charAt(4)); // 4 is my k
+console.log(string2+'a');
+
+let a='Ja'
+let b='len'
+console.log(`Hello ${a + b}`);
