198cfda9d7c86e72736e8c53298253c58f4ece29
Jalen Staples <jastaples@davidson.edu>
2020-02-13 14:28:57 -0800

    2nd revised attempt

--- strings.js
+++ strings.js
@@ -9,15 +9,15 @@ let str3=string1 + string2
 console.log(string('mom', 'dad'));
 console.log(str1.includes(str2));
 
- string2='waffles';
-console.log(string2.length);
-console.log (string2.toUpperCase());
-console.log(string2.toLowerCase());
-console.log(string2.charAt(0));
-console.log(string2.charAt(1));
-console.log(string2.charAt(6));
-console.log(string2.charAt(4)); // 4 is my k
-console.log(string2+'a');
+ waffles='cookies';
+console.log(waffles.length);
+console.log (waffles.toUpperCase());
+console.log(waffles.toLowerCase());
+console.log(waffles.charAt(0));
+console.log(waffles.charAt(1));
+console.log(waffles.charAt(6));
+console.log(waffles.charAt(4)); // 4 is my k
+console.log(waffles+'a');
 
 let a='Ja'
 let b='len'
