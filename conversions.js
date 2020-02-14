46bf2c84ebeb81152e6c1f85edc89b63222a3ba9
Jalen Staples <jastaples@davidson.edu>
2020-02-14 13:04:13 -0800

    Conversions completed

---
+++ conversions.js
@@ -0,0 +1,12 @@
+let num=1234;
+//Number.toString(num);
+console.log('This is converting the number to a string:', num.toString());
+let strnum='1234';
+console.log('This is 1234 as a string:', strnum);
+console.log('This is 1234 as number;', parseInt(strnum));
+let word='apples'
+console.log('This is coverting string to array:', word.split(''));
+let set=['one', '--two--', 'three']
+console.log('This is multiple strings converted to one in an array:', set.join(''));
+let set2=['one', 'two', 'three'];
+console.log('This is multiple strings converted to one (|)in an array:', set2.join('|'));
