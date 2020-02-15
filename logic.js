d2727b2becac9c352fb9f25134530b8459af53c4
Koumpotis Christos <chkoumpotis@davidson.edu>
2020-02-14 16:23:03 -0800

    logical operators

---
+++ logic.js
@@ -0,0 +1,14 @@
+let num1 = 123;
+let num2 = 124;
+
+if (num1 % 2 === 0 && num2 % 2 ===0){
+  console.log('both are even');
+} else if (num1 % 2 ===0 || num2 % 2 ===0){
+  console.log('one of them is even');
+} else {
+  console.log('None of them is even.')
+}
+
+if (num1 !== 0){
+  console.log('num1 is not equal to 0');
+}
