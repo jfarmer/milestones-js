28cb9aed2b2a6003ddb44f5bf513135af8969ba2
Jalen Staples <jastaples@davidson.edu>
2020-02-17 18:35:34 -0800

    First loop activity done

---
+++ loopNumbers.js
@@ -0,0 +1,32 @@
+for(let i=40; i<=60; i++){
+  console.log(i);
+}
+for(let i=40; i<=60; i+=2) {
+  console.log('This is all even integers:', i);
+}
+let min=10;
+let max=20;
+for(let i=min; i<=max; i++) {
+  console.log(i);
+}
+let min1=10;
+let max1=20;
+for(let i=min; i<max; i+=2 ) {
+  console.log('This is all even integers:', i);
+}
+
+let num=7;
+for(let i=1; i<=10; i++) {
+
+  console.log(i*num);
+}
+
+let num1=7;
+for(let i=1; i<=5; i++) {
+  console.log('k=5', i*num1);
+}
+
+let num2=3;
+for(let i=1; i<=4; i++) {
+  console.log('k=4, n=3', i=i*num2);
+}
