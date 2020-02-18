848678e5c7b206f0f4920d1b1de07693d9ff6653
Patrick Sullivan <pasullivan@davidson.edu>
2020-02-17 22:42:55 -0800

    navigation

---
+++ navigtion.js
@@ -0,0 +1,26 @@
+EDIT
+ Console.log("On a Mac: search for terminal in spotlight search");
+ Console.log("Click on terminal application: looks like black monitor with "<_" on it");
+ console.log();
+ console.log("To navigate to a directory such as desktop: ");
+cd desktop
+
+console.log("To navigate to a directory such as desktop...");
+console.log("Will print a list of the file contents of that directory: ");
+ ls
+
+
+console.log("Will create a new empty directory at this location, in this case called new: ");
+
+ mkdir new
+
+ console.log("Will create a new empty directory at this location, in this case called new: ");
+
+ code
+
+ console.log("Will open VS Code from your terminal: ");
+ console.log("Will create a clone of this repository, given the correct URL, at this location ");
+
+ git clone *URL*
+
+ console.log("Make sure to press enter after each of these terminal commands!");
